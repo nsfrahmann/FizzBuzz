292da81179bacdfb9928f4f14943330cd8317d48
nsfrahmann <71308487+nsfrahmann@users.noreply.github.com>
2020-10-09 13:13:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FizzBuzz
+ This is a coding challenge!
---
+++ Scripts/Custom.js
@@ -0,0 +1,50 @@
+// JavaScript source code
+
+//This is an optimal way to code the FizzBuzz Challenge
+function FizzBuzz(val1, val2) {
+    let returnArray = [];
+    for (let i = 1; i <= 100; i++) {
+        returnArray[i] = ((i % val1 == 0 ? 'Fizz' : '') + (i % val2 == 0 ? 'Buzz' : '') || i);
+    }
+    return returnArray;
+}
+
+function FizzBuzzter() {
+    //Grab the template from the HTML document
+    let output = [];
+    let headTemplate = document.getElementById('template-header');
+    let rowTemplate = document.getElementById('template-row-items');
+
+    var templateHTML = rowTemplate.innerHTML;
+    var resultsHTML = headTemplate.innerHTML;
+    //Get the values I want to use
+    let val1 = document.getElementById('Fizzvalue').value;
+    let val2 = document.getElementById('Buzzvalue').value;
+    //Call the Fizz Buzz Function
+    output = FizzBuzz(val1, val2);
+    //Empty value restriction else function output
+    if (val1 == "" || val2 == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'You missed some input!',
+            confirmButtonText: 'Go Back',
+        })
+    } else {
+        for (i = 1; i < output.length; i += 5) {
+            resultsHTML += templateHTML.replace('{{val1}}', output[i])
+                .replace('{{val2}}', output[i + 1])
+                .replace('{{val3}}', output[i + 2])
+                .replace('{{val4}}', output[i + 3])
+                .replace('{{val5}}', output[i + 4]);
+        }
+        document.getElementById('results').innerHTML = resultsHTML;
+        $("#exampleModal").modal();
+    }
+}
+//Input restriction for just numbers
+$(".numOnly").keypress(function (e) {
+    if (!(e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
+        return false;
+    };
+});
